4a86a73a362c4ec73bcc55ebfac9e0937a5bf6bb
Morita Yasuaki <zousandazou@gmail.com>
2013-02-20 16:44:30 +0900

    hom

---
+++ hom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find argen-o-000/*.jpg | xargs -n 20 ./hom img/o-000-
+find argen-o-500/*.jpg | xargs -n 20 ./hom img/o-500-
+find argen-o-1150/*.jpg | xargs -n 20 ./hom img/o-1150-
+find argen-o-1700/*.jpg | xargs -n 20 ./hom img/o-1700-
+find argen-o-2500/*.jpg | xargs -n 20 ./hom img/o-2500-
+find argen-o-3200/*.jpg | xargs -n 20 ./hom img/o-3200-
+find argen-o-4000/*.jpg | xargs -n 20 ./hom img/o-4000-
+find argen-o-4700/*.jpg | xargs -n 20 ./hom img/o-4700-
